bb3e0c2b8cef1e1286d4b270a61ef07bce63965e
ibrahim0891 <nijhum0891@gmail.com>
2024-11-14 18:42:30 +0600

    d

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config(); 
+require("dotenv").config();
 const AuthRoute = require("./routes/AuthRoutes");
 const todoRoutes = require("./routes/todoRoutes");
 const database = require("./config/db");
 const { default: mongoose } = require("mongoose");
 const UserRouter = require("./routes/UserRoutes");
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,9 +17,9 @@ app.use(express.json());
 const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl).then((result) => {
-  console.log("Connected to database");
+    console.log("Connected to database");
 }).catch((err) => {
-  console.log(err);
+    console.log(err);
 });
 
 app.use("/api/user", AuthRoute);
@@ -27,7 +27,7 @@ app.use("/api/todos", todoRoutes);
 app.use("/api/u", UserRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+    res.send("Hello");
 });
 
 //testing branch 
@@ -35,5 +35,5 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 3004;
 
 app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
+    console.log(`App is listening on port ${port}`);
 });
