79f40e1f27209bf99cbd893333621a01e6b81285
ibrahim0891 <nijhum0891@gmail.com>
2024-11-23 22:55:38 +0600

    fixed some weird bugs

--- src/controller/FriendController.js
+++ src/controller/FriendController.js
@@ -1,134 +1,158 @@
 const User = require("../model/userModel");
 
 const getUsers = async (req, res) => {
-  try {
-    const uid = req.headers["uid"];
-    const users = await User.find({ _id: { $ne: uid } });
-    if (!users) {
-      return res.send("No User Found Insted of You");
-    }
-    res.send(users);
-  } catch (error) {}
+    try {
+        const uid = req.headers["uid"];
+        console.log(uid);
+        const users = await User.find({ _id: { $ne: uid } });
+        if (!users) {
+            return res.send("No User Found Insted of You");
+        }
+        console.log(users);
+        res.send(users);
+    } catch (error) { }
 };
+
 
 const addFriends = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const uid = req.headers["uid"];
+    try {
+        const { id } = req.body;
+        const uid = req.headers["uid"];
 
-    if (!uid || !id) {
-      return res.send({ message: "UID and Friend ID are required" });
+        if (!uid || !id) {
+            return res.send({ message: "UID and Friend ID are required" });
+        }
+
+        const Me = await User.findOne({ _id: uid });
+        const Friend = await User.findOne({ _id: id });
+
+        if (!Me || !Friend) {
+            res.send({ message: "User not found" });
+        }
+
+        if (!Me.sentFR.includes(id)) {
+            Friend.pendingFR.push(uid);
+            Me.sentFR.push(id);
+        } else {
+            return res.send({ message: "Already Sent Request" })
+        }
+        if (Me.friends.includes(Friend._id)) {
+            return res.send({ message: "Already friends" });
+        }
+
+        await Me.save();
+        await Friend.save();
+
+    await res.status(201).send({ message: "Request Sent" });
+    } catch (error) {
+        console.log(error);
     }
-
-    const Me = await User.findOne({ _id: uid });
-    const Friend = await User.findOne({ _id: id });
-
-    if (!Me || !Friend) {
-      res.send("User not found");
-    }
-
-    if (!Me.pendingFR.includes(id)) {
-      Me.pendingFR.push(id);
-      Friend.sentFR.push(uid);
-    }
-
-    await Me.save();
-    await Friend.save();
-
-    await res.send(Me, { FrStatus: "pending" });
-  } catch (error) {
-    console.log(error);
-  }
 };
-
 const acceptReq = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const uid = req.headers["uid"];
+    try {
+        const { id } = req.body;
+        const uid = req.headers["uid"];
 
-    if (!uid || !id) {
-      return res.send({ message: "UID and Friend ID are required" });
+        if (!uid || !id) {
+            return res.send({ message: "UID and Friend ID are required" });
+        }
+
+        const Me = await User.findOne({ _id: uid });
+        const Friend = await User.findOne({ _id: id });
+
+        if (!Me || !Friend) {
+            res.send("User not found");
+        }
+
+        if (Me.friends.includes(Friend._id)) {
+            return res.send({ message: "Already friends" });
+        }
+
+        Me.friends.push(Friend._id);
+        Friend.friends.push(Me._id);
+
+        Me.pendingFR = Me.pendingFR.filter((fr) => fr.toString() !== id);
+        Friend.sentFR = Friend.sentFR.filter(
+            (fr) => fr.toString() !== Me._id.toString()
+        );
+
+        await Me.save();
+        await Friend.save();
+
+        await res.status(200).send('Friend Request Accepted');
+    } catch (error) {
+        console.log(error);
     }
-
-    const Me = await User.findOne({ _id: uid });
-    const Friend = await User.findOne({ _id: id });
-
-    if (!Me || !Friend) {
-      res.send("User not found");
-    }
-
-    Me.friends.push(Friend._id);
-    Friend.friends.push(Me._id);
-
-    Me.pendingFR = Me.pendingFR.filter((fr) => fr.toString() !== id);
-    Friend.sentFR = Me.sentFR.filter(
-      (fr) => fr.toString() !== Me._id.toString()
-    );
-
-    await Me.save();
-    await Friend.save();
-
-    await res.send(Me, { FrStatus: "friends" });
-  } catch (error) {
-    console.log(error);
-  }
 };
-
 const cancelReq = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const uid = req.headers["uid"];
+    try {
+        const { id } = req.body;
+        const uid = req.headers["uid"];
 
-    if (!uid || !id) {
-      return res.send({ message: "UID and Friend ID are required" });
+        if (!uid || !id) {
+            return res.send({ message: "UID and Friend ID are required" });
+        }
+
+        const Me = await User.findOne({ _id: uid });
+        const Friend = await User.findOne({ _id: id });
+
+        if (!Me || !Friend) {
+            res.send("User not found");
+        }
+
+        Friend.pendingFR = Friend.pendingFR.filter((fr) => fr.toString() !== uid);
+        Me.sentFR = Me.sentFR.filter(
+            (fr) => fr.toString() !== Friend._id.toString()
+        );
+
+        await Me.save();
+        await Friend.save();
+        await res.send({ message: "Request Cancelled" });
+    } catch (error) {
+        console.log(error);
     }
-
-    const Me = await User.findOne({ _id: uid });
-    const Friend = await User.findOne({ _id: id });
-
-    if (!Me || !Friend) {
-      res.send("User not found");
-    }
-
-    Me.pendingFR = Me.pendingFR.filter((fr) => fr.toString() !== id);
-    Friend.sentFR = Me.sentFR.filter(
-      (fr) => fr.toString() !== Me._id.toString()
-    );
-
-    await Me.save();
-    await Friend.save();
-    await res.send(Me, { FrStatus: "add friend" });
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 const deleteFriend = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const uid = req.headers["uid"];
+    try {
+        const { id } = req.params;
+        const uid = req.headers["uid"];
 
-    if (!uid || !id) {
-      return res.send({ message: "UID and Friend ID are required" });
+        if (!uid || !id) {
+            return res.send({ message: "UID and Friend ID are required" });
+        }
+
+        const Me = await User.deleteOne({ _id: uid });
+        const Friend = await User.findOne({ _id: id });
+
+        if (!Me || !Friend) {
+            res.send("User not found");
+        }
+        Me.friends = Me.friends.filter((fr) => fr.toString() !== id);
+        Friend.friends = Me.friends.filter(
+            (fr) => fr.toString() !== Me._id.toString()
+        );
+
+        await Me.save();
+        await Friend.save();
+        await res.send(Me, { FrStatus: "add friend" });
+    } catch (error) {
+        console.log(error);
     }
-
-    const Me = await User.deleteOne({ _id: uid });
-    const Friend = await User.findOne({ _id: id });
-
-    if (!Me || !Friend) {
-      res.send("User not found");
-    }
-    Me.friends = Me.friends.filter((fr) => fr.toString() !== id);
-    Friend.friends = Me.friends.filter(
-      (fr) => fr.toString() !== Me._id.toString()
-    );
-
-    await Me.save();
-    await Friend.save();
-    await res.send(Me, { FrStatus: "add friend" });
-  } catch (error) {
-    console.log(error);
-  }
 };
 
-module.exports = { getUsers, addFriends, deleteFriend, acceptReq, cancelReq };
+
+const getFriends = async (req, res) => {
+    try {
+        const uid = req.headers["uid"];
+        const user = await User.findOne({ _id: uid }).populate("friends");
+        if (!user) {
+            return res.send("User not found");
+        }
+        res.send(user.friends);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = {getFriends, getUsers, addFriends, deleteFriend, acceptReq, cancelReq };
--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,30 +1,30 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        token: {
+            type: String,
+            required: false,
+        },
+        friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+        pendingFR: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+        sentFR: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    token: {
-      type: String,
-      required: false,
-    },
-    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    pendingFR: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    sentFR: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  },
 
-  { timestamps: true }
+    { timestamps: true }
 );
 
 const User = new mongoose.model("User", userSchema);
