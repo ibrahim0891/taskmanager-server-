e96f47b3f341c8f721fdc4aec143140da0a28ab3
ibrahim0891 <nijhum0891@gmail.com>
2024-11-26 11:13:20 +0600

    added socket

--- src/index.js
+++ src/index.js
@@ -1,45 +1,67 @@
-// server.js
+
+//Dependencies
 const express = require("express");
-const app = express();
 const cors = require("cors");
-require("dotenv").config();
+const http = require('http')
+const socket = require('socket.io')
+const { mongoose } = require("mongoose");
+
+//Routes 
 const AuthRoute = require("./routes/AuthRoutes");
 const todoRoutes = require("./routes/todoRoutes");
-const database = require("./config/db");
-const { default: mongoose } = require("mongoose");
 const UserRouter = require("./routes/UserRoutes");
 const ProfileRouters = require("./routes/ProfileRoutes");
 const FriendRoute = require("./routes/FriendRoutes");
 
+//Dependency Initialization
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3003;
+
+
+//middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//server and database initialization
+const server = http.createServer(app)
 const dbUrl = process.env.DB_URL;
 
 mongoose
-  .connect(dbUrl)
-  .then((result) => {
-    console.log("Connected to database");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(dbUrl)
+    .then((result) => {
+        console.log("Connected to database");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
+//Route Middleware
 app.use("/api/user", AuthRoute);
 app.use("/api/todos", todoRoutes);
 app.use("/api/u", UserRouter);
 app.use("/api/profile", ProfileRouters);
 app.use("/api/friends", FriendRoute);
 
+
+
+//server Rollout 
 app.get("/", (req, res) => {
-  res.send("Hello");
+    res.send("Hello");
 });
 
-//testing branch
 
-const port = process.env.PORT || 3003;
+const io = socket(server, {
+    cors: {
+        origin: '*'
+    }
+})
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
+io.on('connection', (socket) => {
+    console.log(socket.id);
+})
+
+server.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
 });
